56669e4f0bcfd940f6ad7cfb07e684d2f6cae63e
Irvin Cueto <iirvin.cueto.02@gmail.com>
2022-12-08 09:34:52 -0600

    Final2

--- resources/views/cliente/interfazCliente.blade.php
+++ resources/views/cliente/interfazCliente.blade.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')-Cliente</title>
+
     <link rel="icon" href="{{asset('img/admin/inicio/logoEM.png')}}">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
@@ -14,7 +16,7 @@
     </script>
     <link rel="stylesheet" href="{{ asset('css/interfacesCliente/index.css') }}">
 </head>
- 
+
 
 <header class="d-flex justify-content-around ">
     <div class="" id="NumMesa">
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -12,7 +12,7 @@
             }
             .min-h-screen{
                 background-image: url('/img/cliente/background.png');
-              
+
 
             }
 
